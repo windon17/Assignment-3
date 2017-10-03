e6cee1d8445df0bbb1af31a569bfeb13763e0e4d
Katrina <windon@uark.edu>
2017-10-03 13:43:39 -0500

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("li #img").mouseover(function(){
+        $("li #img").css("height", "200px");
+    });
+    $("li #img").mouseout(function(){
+        $("li #img").css("height", "100px");
+   });
+
+})
+
